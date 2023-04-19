438d3796595a37d549131b35dad19a667730ecfe
Long Tran <longdtranw@gmail.com>
2023-04-19 11:33:58 -0600

    finished

--- shellcode/shellcode_tester.c
+++ shellcode/shellcode_tester.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
 
-    char exec_me[1000];
+    char exec_me[200];
     int bytes_read;
     FILE * shellcode_file;
     void* foo;
@@ -18,13 +18,21 @@ int main(int argc, char** argv) {
         return 2;
     }
 
-    bytes_read = fread(exec_me, 1, 1000, shellcode_file);
+    bytes_read = fread(exec_me, 1, 200, shellcode_file);
 
     printf("Shellcode length: %ld\n", ftell(shellcode_file));
 
     if (!bytes_read) {
         printf("Error: %d bytes read in shellcode file %s\n", bytes_read, SHELLCODE_PATH);
         return 1;
+    }
+
+    for (int i = 0; i < bytes_read; ++i) {
+        if (exec_me[i] == 0) {
+            printf("code contain null bytes %d\n", i);
+            exit(1);
+        }
+            
     }
 
     pclose(shellcode_file);
